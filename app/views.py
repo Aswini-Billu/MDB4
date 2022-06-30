3a38ba74f078e407a8c0e9ebd572ccf9dd9d48e4
AshwinSreeYadav <aswinibillu5@gmail.com>
2022-06-30 22:01:03 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def aswini(request):
     return render(request,'mdb4.html')
+def sandy(request):
+    return render(request,'child.html')
--- project17/urls.py
+++ project17/urls.py
@@ -20,4 +20,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('aswini/',aswini,name='aswini'),
+    path('sandy/',sandy,name='sandy'),
 ]
